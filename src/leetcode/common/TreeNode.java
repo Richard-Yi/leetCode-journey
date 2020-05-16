e66ad99475884e8ef557d3a5047b50f5497d2329
Richard <yyf_richard@outlook.com>
2020-05-16 18:35:35 +0800

    增加二叉树数据结构

---
+++ src/leetcode/common/TreeNode.java
@@ -0,0 +1,12 @@
+package leetcode.common;
+
+/**
+ * @author Richard_yyf
+ * @version 1.0 2020/5/16
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
