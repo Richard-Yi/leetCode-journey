8b4a959cbe1ac9bdbceff7f6c19f6b72e88c546e
Richard_yyf <yyf_richard@outlook.com>
2021-05-18 20:41:29 +0800

    - T04 重写提交，tags: 数据,二分法

--- src/self/learn/array/FindMedianSortedArrays_T4.java
+++ src/self/learn/array/FindMedianSortedArrays_T4.java
@@ -20,6 +20,9 @@ public class FindMedianSortedArrays_T4 {
         System.out.println("real:" + findMedianSortedArrays(new int[]{}, new int[]{1}));
         System.out.println("expected: " + 1);
         System.out.println("================================");
+        System.out.println("real:" + findMedianSortedArrays(new int[]{}, new int[]{2, 3}));
+        System.out.println("expected: " + 2.5);
+        System.out.println("================================");
         System.out.println("real:" + findMedianSortedArrays(new int[]{}, new int[]{1, 2, 3}));
         System.out.println("expected: " + 2);
         System.out.println("================================");
@@ -37,45 +40,52 @@ public class FindMedianSortedArrays_T4 {
     }
 
     static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int l1 = nums1.length;
-        int l2 = nums2.length;
-        int l = l1 + l2;
+        int len1 = nums1.length;
+        int len2 = nums2.length;
 
-        if (l1 > l2) {
+        if (len1 > len2) {
             return findMedianSortedArrays(nums2, nums1);
         }
 
-        if (l1 == 0) {
-            return (l2 % 2  == 0) ? (nums2[l2 / 2] + nums2[(l2 - 1)/ 2]) / 2.0 : nums2[(l2 - 1) / 2];
+        if (len1 == 0) {
+            if (len2 % 2 == 0) {
+                return (nums2[len2 / 2] + nums2[(len2 - 1) / 2]) / 2.0;
+            } else {
+                return nums2[len2 / 2];
+            }
         }
 
-        int start1 = 0;
-        int end1 = nums1.length; // TODO 这里没理解
-        // 分界线
-        int cut1, cut2;
-        // 二分法，l1 <= r2, l2 <= r1 范围内
-        while (start1 <= end1) {
-            cut1 = (start1 + end1) / 2;
-            cut2 = (l + 1) / 2 - cut1; // TODO 这里没理解
-            double left1 = cut1 == 0 ? Integer.MIN_VALUE : nums1[cut1 - 1];
-            double left2 = cut2 == 0 ? Integer.MIN_VALUE : nums2[cut2 - 1];
-            double right1 = cut1 == l1 ? Integer.MAX_VALUE : nums1[cut1];
-            double right2 = cut2 == l2 ? Integer.MAX_VALUE : nums2[cut2];
-            if (left2 > right1) {
-                // 往右
-                start1 = cut1 + 1;
-            } else if (left1 > right2) {
-                // 往左
-                end1 = cut1 - 1;
+        int len = len2 + len1;
+        int start = 0;
+        int end = len1; // 懂了，因为是代表partition,而不是那个Index
+        int partition1;
+        int partition2;
+        while (start <= end) {
+            // 确定分割线位置
+            partition1 = (start + end) / 2;
+            partition2 = (len + 1) / 2 - partition1;
+            // 计算分割线两边的值用于二分法结果条件判断
+            double left1 = partition1 == 0 ? Integer.MIN_VALUE : nums1[partition1 - 1];
+            double left2 = partition2 == 0 ? Integer.MIN_VALUE : nums2[partition2 - 1];
+            double right1 = partition1 == len1 ? Integer.MAX_VALUE : nums1[partition1];
+            double right2 = partition2 == len2 ? Integer.MAX_VALUE : nums2[partition2];
+            if (left1 > right2) {
+                // partition1 需要往左移动，让left1变小
+                // 数组1的分割线要往左移动
+                end = partition1 - 1;
+            } else if (right1 < left2) {
+                // partition2 需要往右移动，让right1变大
+                // 数组1的分割线要往右移动
+                start = partition1 + 1;
             } else {
-                // 到了
-                if (l % 2 == 0) {
+                if (len % 2 == 0) {
                     return (Math.max(left1, left2) + Math.min(right1, right2)) / 2.0;
                 } else {
                     return Math.max(left1, left2);
                 }
             }
         }
+
         return -1;
     }
 }
