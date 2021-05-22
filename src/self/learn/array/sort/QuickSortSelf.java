ee64b265376e2155db7eb933cc6992aa5f073f03
Richard_yyf <yyf_richard@outlook.com>
2021-05-22 20:35:31 +0800

    提交自己手写的快速排序算法

--- src/self/learn/array/sort/QuickSortSelf.java
+++ src/self/learn/array/sort/QuickSortSelf.java
@@ -35,7 +35,10 @@ public class QuickSortSelf {
 
     private int partition(int[] nums, int left, int right) {
         int pivot = nums[right];
-
+        // 两个指针分别是 j 和 i，j 会一直向右顺序遍历，并且这个过程中会一直与 pivot 位置的数比较。
+        // 只有当 j 位置的数小于 pivot 位置的数才会发生和 i 位置的交换（也就是swap A[ i ] with A [ j ]），
+        // 同时 i 自增一次。直到遍历完成后， i 左边的数就都是小于pivot的，
+        // 此时把 i 位置的数和pivot位置的数交换，那么pivot左边就都是比它小的数，而右边就都是比它大的数了,这样我们就成功完成了一个原地排序算法！
         int i = left;
         for (int j = left; j < right; j++) {
             if (nums[j] < pivot) {
