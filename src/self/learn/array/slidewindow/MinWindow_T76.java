258657d5b6f64a4aa7d874a5ce28470fee28277d
Richard_yyf <yyf_richard@outlook.com>
2021-05-25 20:32:44 +0800

    MinWindow_T76 - tag: 滑动窗口 - 难度：hard

--- src/self/learn/array/slidewindow/MinWindow_T76.java
+++ src/self/learn/array/slidewindow/MinWindow_T76.java
@@ -15,62 +15,70 @@ public class MinWindow_T76 {
 
     }
     public String minWindow(String s, String t) {
+
         if (t.length() > s.length()) {
             return "";
         }
 
-        Map<Character, Integer> target = new HashMap<>(t.length());
+        Map<Character, Integer> targetMap = new HashMap<>(t.length());
+        Map<Character, Integer> windowMap = new HashMap<>(t.length());
 
+        // 初始化 targetMap
         for (int i = 0; i < t.length(); i++) {
-            target.put(t.charAt(i), target.getOrDefault(t.charAt(i), 0) + 1);
+            targetMap.put(t.charAt(i), targetMap.getOrDefault(t.charAt(i), 0) + 1);
         }
 
+
+        // 初始化
         int left = 0;
         int right = t.length() - 1;
-        int valid = 0;
-        Map<Character, Integer> window = new HashMap<>(s.length());
-        String targetStr = null;
-        for(int i = 0; i < t.length(); i++) {
-            if (target.containsKey(s.charAt(i))) {
-                int count = window.getOrDefault(s.charAt(i), 0);
-                if (count < target.get(s.charAt(i))) {
-                    valid++;
+        int matchCount = 0;
+        for (int i = 0; i <= right; i++) {
+            if (targetMap.containsKey(s.charAt(i))) {
+                // 如果包含则放进去，并且+1
+                windowMap.put(s.charAt(i), windowMap.getOrDefault(s.charAt(i), 0) + 1);
+                if (windowMap.get(s.charAt(i)) <= targetMap.get(s.charAt(i))) {
+                    // 如果+1了之后，小于等于目标数量，则说明 +1有效
+                    matchCount++;
                 }
-                window.put(s.charAt(i), count + 1);
             }
         }
 
+        // 最小窗口的left
+        int minWindowStart = 0;
+        int minWindowSize = Integer.MAX_VALUE;
         while (true) {
-            if (valid == t.length()) {
-                // 更新 targetStr
-                if (targetStr == null || targetStr.length() > right - left + 1) {
-                    targetStr = s.substring(left, right + 1);
+            if (matchCount == t.length()) {
+                // 满足条件，判断再更新
+                if (right - left + 1 < minWindowSize) {
+                    minWindowStart = left;
+                    minWindowSize = right - left + 1;
                 }
-                // left 右移 right 不变
-                if (target.containsKey(s.charAt(left))) {
-                    window.put(s.charAt(left), window.get(s.charAt(left)) - 1);
-                    if (window.get(s.charAt(left)) < target.get(s.charAt(left))) {
-                        valid--;
+                // right 不变，left 右移
+                if (targetMap.containsKey(s.charAt(left))) {
+                    windowMap.put(s.charAt(left), windowMap.getOrDefault(s.charAt(left), 0) - 1);
+                    // 如果 -1了之后，小于目标数量，则说明 -1 有效
+                    if (windowMap.get(s.charAt(left)) < targetMap.get(s.charAt(left))) {
+                        matchCount--;
                     }
                 }
                 left++;
-            } else if (valid < t.length()){
-                // right 右移，left 不变
+            } else if (matchCount < t.length()) {
                 if (right == s.length() - 1) {
                     break;
                 }
+
+                // 不满足，right 右移，left不动
                 right++;
-                if (target.containsKey(s.charAt(right))) {
-                    int count = window.getOrDefault(s.charAt(right), 0);
-                    if (count < target.get(s.charAt(right))) {
-                        valid++;
+                if (targetMap.containsKey(s.charAt(right))) {
+                    windowMap.put(s.charAt(right), windowMap.getOrDefault(s.charAt(right), 0) + 1);
+                    if (windowMap.get(s.charAt(right)) <= targetMap.get(s.charAt(right))) {
+                        // 如果+1了之后，小于等于目标数量，则说明 +1有效
+                        matchCount++;
                     }
-                    window.put(s.charAt(right), count + 1);
                 }
             }
         }
-
-        return targetStr == null ? "" : targetStr;
+        return minWindowSize == Integer.MAX_VALUE ? "" : s.substring(minWindowStart, minWindowStart + minWindowSize);
     }
-
 }
