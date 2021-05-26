192725681e419d97357d4a5c54fdeb62949407d5
Richard_yyf <yyf_richard@outlook.com>
2021-05-26 22:27:37 +0800

    RemoveElements_T203 - tag: 链表 - 难度：easy

---
+++ src/self/learn/linklist/RemoveElements_T203.java
@@ -0,0 +1,26 @@
+package self.learn.linklist;
+
+import leetcode.common.ListNode;
+
+/**
+ * @author Richard_yyf
+ * @version 1.0 2021/5/26
+ */
+public class RemoveElements_T203 {
+
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode cur = dummy.next;
+        while (cur != null) {
+            if (cur.val == val) {
+                pre.next = cur.next;
+            } else {
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+}
